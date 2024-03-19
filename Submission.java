6f136b3a8c15933d094a9ffccfc3892c4ce224d8
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2024-03-19 22:07:35 +0000

    "Commit"

--- Submission.java
+++ Submission.java
@@ -3,22 +3,45 @@ import java.util.ArrayList;
 
 public class Submission {
 
-  public Submission() {
-    BinarySearchTree binarySearchTree = new BinarySearchTree();
-  }
 
-  public class Node {
-    private int val;
-    private Node left;
-    private Node right;
-  }
+  public static class BinarySearchTree {
 
-  public class BinarySearchTree {
+    public static int deleted = 0;
+    public static int inserted = 0;
+
     ArrayList<Node> BinarySearchTree;
+
+    private Node root;
+
+    private class Node {
+      private int val;
+      private Node left;
+      private Node right;
+      private int count;
+
+      public Node(int val) {
+
+        this.val = val;
+
+        this.left = null;
+        this.right = null;
+        this.count = 1;
+
+      }
+    }
+
+    public void printElement() {
+      System.out.println(String.format("%s(%s)", this.root.val, this.root.count));
+      System.out.println();
+
+    }
   }
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+    while (true) {
     String startMessage = """
         1. Insert an element into the BST
         2. Search for an element in the BST
@@ -30,8 +53,47 @@ public class Submission {
         8. Delete an element
         0. To exit the program
         """;
+    System.out.println(startMessage);
+    String response = scanner.nextLine();
 
-    scanner.close();
+    switch (response) {
+      case "1":
+        System.out.println("Enter the element to insert:");
+        int elementToInsert = scanner.nextInt();
+
+        break;
+      case "2":
+        // Implement searching
+        binarySearchTree.printElement();
+        break;
+      case "3":
+        // Implement finding maximum
+        break;
+      case "4":
+        // Implement finding minimum
+        break;
+      case "5":
+        // Implement printing preorder
+        break;
+      case "6":
+        // Implement printing postorder
+        break;
+      case "7":
+        // Implement printing inorder
+        break;
+      case "8":
+        System.out.println("Enter the element to delete:");
+
+        break;
+      case "0":
+        scanner.close();
+        return; // Exit the program
+      default:
+        System.out.println("Invalid input! Please enter a number between 0 and 8.");
+    }
+    ;
+
+  }
 
   }
 
