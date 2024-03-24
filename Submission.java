0a549e51b70fe5859ddf123605d652b210068db2
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2024-03-24 17:47:39 +0000

    "Commit"

--- Submission.java
+++ Submission.java
@@ -1,16 +1,9 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Submission {
+
   public static class BinarySearchTree {
-
-
     private Node root;
-
-    public BinarySearchTree() {
-      root = null;
-    }
 
     private class Node {
       private int val;
@@ -26,166 +19,169 @@ public class Submission {
       }
     }
 
+    public BinarySearchTree() {
+      root = null;
+    }
+
     public void insert(int val) {
       root = insertHelper(root, val);
     }
 
-    public Node insertHelper(Node root, int val) {
+    private Node insertHelper(Node root, int val) {
       if (root == null) {
         root = new Node(val);
         return root;
       }
 
-      if (val <= root.val) {
+      if (root.val == val) {
+        root.count++;
+      } else if (val < root.val) {
         root.left = insertHelper(root.left, val);
       } else {
         root.right = insertHelper(root.right, val);
       }
       return root;
-
-    }
-
-    public void displayNodes() {
-      displayNodesHelper(root);
-    }
-
-    public void displayNodesHelper(Node root) {
-      if (root != null) {
-        System.out.print(root.val + "(" + root.count + ")" + " ");
-        displayNodesHelper(root.left);
-        displayNodesHelper(root.right);
-      }
-
     }
 
     public void binarySearch(int val) {
-      boolean found = binarySearchHelper(root, val);
-      if (found == true)
-        System.out.println("Found " + val);
-      else
-        System.out.println("Not Found" + val);
+      Node found = binarySearchHelper(root, val);
+      if (found != null) {
+        System.out.println(found.val + "(" + found.count + ")");
+      } else {
+        System.out.println(val + "(0)");
+      }
     }
 
-    public boolean binarySearchHelper(Node root, int val) {
+    private Node binarySearchHelper(Node root, int val) {
       if (root == null) {
-        return false;
+        return null;
       }
-      if (val == root.val)
-        return true;
-      else if (val <= root.val) {
+      if (val == root.val) {
+        return root;
+      } else if (val < root.val) {
         return binarySearchHelper(root.left, val);
       } else {
         return binarySearchHelper(root.right, val);
       }
     }
 
-    public void deleteNode() {
+    public Node maxNode() {
+      return maxNodeHelper(root);
     }
 
-    public void deleteNodeHelper(Node root, int val) {
-      if (root == null) {
-        System.out.println("No Found");
-        return;
-      }
-    }
-
-    public int maxNode() {
-      int max = maxNodeHelper(root);
-      return max;
-    }
-
-    public int maxNodeHelper(Node node) {
+    private Node maxNodeHelper(Node node) {
       if (node == null) {
-        return 0;
+        return null;
       }
-
       if (node.right == null) {
-        return node.val;
+        return node;
       }
       return maxNodeHelper(node.right);
     }
 
-    public int minNode() {
-      int min = minNodeHelper(root);
-      return min;
+    public Node minNode() {
+      return minNodeHelper(root);
     }
 
-    public int minNodeHelper(Node node) {
+    private Node minNodeHelper(Node node) {
       if (node == null) {
-        return 0;
+        return null;
       }
       if (node.left == null) {
-        return node.val;
+        return node;
       }
       return minNodeHelper(node.left);
     }
 
-    // Binary Search on the Node of the Tree
+    public void printPreOrder() {
+      printPreOrderHelper(root);
+    }
 
-    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      BinarySearchTree binarySearchTree = new BinarySearchTree();
-
-      while (true) {
-        String startMessage = """
-            1. Insert an element into the BST
-            2. Search for an element in the BST
-            3. Find the maximum element from the BST
-            4. Find the minimum element from the BST
-            5. Print the elements in the BST in preorder
-            6. Print the elements in the BST in postorder
-            7. Print the elements in the BST in inorder
-            8. Delete an element
-            0. To exit the program
-            """;
-
-        System.out.println(startMessage);
-        String response = scanner.nextLine();
-
-        switch (response) {
-          case "1":
-
-            int elementToInsert = scanner.nextInt();
-            binarySearchTree.insert(elementToInsert);
-
-            break;
-          case "2":
-            // Implement searching
-            int elementToFind = scanner.nextInt();
-            binarySearchTree.binarySearch(elementToFind);
-
-            break;
-          case "3":
-            // Implement finding maximum
-            System.out.println(binarySearchTree.maxNode());
-            break;
-          case "4":
-            // Implement finding minimum
-            System.out.println(binarySearchTree.minNode());
-            break;
-          case "5":
-            // Implement printing preorder
-            break;
-          case "6":
-            // Implement printing postorder
-            break;
-          case "7":
-            // Implement printing inorder
-            break;
-          case "8":
-            System.out.println("Enter the element to delete:");
-            // Implement deletion
-            break;
-          case "0":
-            scanner.close();
-            return; // Exit the program
-          default:
-            binarySearchTree.displayNodes();
-            System.out.println();
-            break;
-
-        }
+    private void printPreOrderHelper(Node root) {
+      if (root == null) {
+        return;
       }
+      System.out.print(root.val + " " + "(" + root.count + ")");
+      printPreOrderHelper(root.left);
+      printPreOrderHelper(root.right);
+    }
+
+    public void printInOrder() {
+      printInOrderHelper(root);
+    }
+
+    private void printInOrderHelper(Node root) {
+      if (root == null) {
+        return;
+      }
+      printInOrderHelper(root.left);
+      System.out.print(root.val + " " + "(" + root.count + ")");
+      printInOrderHelper(root.right);
+    }
+
+    public void printPostOrder() {
+      printPostOrderHelper(root);
+    }
+
+    private void printPostOrderHelper(Node node) {
+      if (node == null) {
+        return;
+      }
+      printPostOrderHelper(node.left);
+      printPostOrderHelper(node.right);
+      System.out.print(node.val + " " + "(" + node.count + ")");
     }
   }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+    while (scanner.hasNext()) {
+      int response = scanner.nextInt();
+
+      switch (response) {
+        case 1:
+          int elementToInsert = scanner.nextInt();
+          binarySearchTree.insert(elementToInsert);
+          break;
+        case 2:
+          int elementToFind = scanner.nextInt();
+          binarySearchTree.binarySearch(elementToFind);
+          break;
+        case 3:
+          BinarySearchTree.Node maxNode = binarySearchTree.maxNode();
+          System.out.print(maxNode != null ? maxNode.val + "(" + maxNode.count + ")" : "0(0)");
+          System.out.println();
+          break;
+        case 4:
+          BinarySearchTree.Node minNode = binarySearchTree.minNode();
+          System.out.print(minNode != null ? minNode.val + "(" + minNode.count + ")" : "0(0)");
+          System.out.println();
+          break;
+        case 5:
+          binarySearchTree.printPreOrder();
+          System.out.println();
+          break;
+        case 6:
+          binarySearchTree.printPostOrder();
+          System.out.println();
+          break;
+        case 7:
+          binarySearchTree.printInOrder();
+          System.out.println();
+          break;
+        case 8:
+          System.out.println("Deletion feature not implemented yet.");
+          break;
+        case 0:
+          scanner.close();
+          return;
+      }
+
+    }
+
+    scanner.close();
+  }
+
 }
